cdc9876335347abab0dc09ec71ac2d5d253a0253
irgendwann2025-refer <irgendwann2025@gmail.com>
2026-01-31 11:34:00 -0500

    Delete Install-Zenmap.ps1

--- Install-Zenmap.ps1
+++
@@ -1,9 +0,0 @@
-[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-$url = "https://nmap.org/dist/nmap-7.95-setup.exe"
-$output = "$env:TEMP\nmap-setup.exe"
-
-Invoke-WebRequest -Uri $url -OutFile $output
-Write-Host "Installing Zenmap/Nmap..." -ForegroundColor Green
-# /S for Silent, /D to specify directory (optional)
-Start-Process -FilePath $output -ArgumentList "/S" -Wait
-Remove-Item $output
